370cda3bf51a302d404ac6f6c197e1134138cdbf
Terex12 <sz.yufanxu@gmail.com>
2021-02-19 11:05:05 -0700

    work on master weight init

--- fairscale/nn/op/DistLinear.py
+++ fairscale/nn/op/DistLinear.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn.init as init
 from torch.nn.parameter import Parameter
-from .utils import divide_and_check_no_remainder
+from .utils import divide_and_check_no_remainder, initialize_affine_weight
 
 class DistLinear(torch.nn.Module):
     """
@@ -12,8 +12,9 @@ class DistLinear(torch.nn.Module):
     X: input
     A: kernel
     Arguments:
-        in_features: first dimension of matrix A (reduction dim).
-        out_features: second dimension of matrix A (out dim).
+        in_features: first dimension of matrix X (input dim)
+        reduce_size: first dimension of matrix A (reduction dim).
+        out_features: second dimension of matrix A (output dim).
         bias: If true, add bias. Note that bias is not parallelized.
         gather_output: If true, call all-gether on output and make Y avaiable
                        to all GPUs, otherwise, every GPU will have its output
@@ -33,19 +34,21 @@ class DistLinear(torch.nn.Module):
         self,
         in_features: int,
         out_features: int,
+        reduce_size: int,
         bias: bool = True,
         gather_output: bool = True,
         input_is_parallel: bool = False,
         init_method: Callable[[torch.Tensor], torch.Tensor] = init.xavier_normal_,
         stride: int = 1,
         keep_master_weight_for_test: bool = False,
-        partition_strategy: List[int] = [1, 1],  #in_dim(reduction_dim), out_dim,
+        partition_strategy: List[int] = [1, 1, 1],  #in_dim, out_dim, reduction_dim
     ) -> None:
         super(ColumnParallelLinear, self).__init__()
 
         # Keep input parameters
         self.in_features = in_features
         self.out_features = out_features
+        self.reduce_size = reduce_size
         self.gather_output = gather_output
         self.input_is_parallel = input_is_parallel
 
@@ -53,19 +56,63 @@ class DistLinear(torch.nn.Module):
 
         self.parti_in_features = partition_strategy[0]
         self.parti_out_features = partition_strategy[1]
-        #self.parti_reduce_features = partition_strategy[2]
+        self.parti_reduce_features = partition_strategy[2]
 
-        assert (self.parti_in_features*self.parti_out_features) \
-               <= world_size, "product of partition can not exceed all nodes number"
+        # how to assert ??
+        # assert (self.parti_in_features*self.parti_out_features) \
+        #        <= world_size, "product of partition can not exceed all nodes number"
 
         self.input_size_per_partition = divide_and_check_no_remainder(in_features, self.parti_in_features)
         self.output_size_per_partition = divide_and_check_no_remainder(out_features, self.parti_out_features)
-        #self.reduce_size_per_partition = divide_and_check_no_remainder(out_features, self.parti_out_features)
+        self.reduce_size_per_partition = divide_and_check_no_remainder(reduce_size, self.parti_reduce_features)
+
+        # Parameters.
+        # Note: torch.nn.functional.linear performs XA^T + b and as a result
+        # we allocate the transpose.
+        self.weight = Parameter(torch.Tensor(self.output_size_per_partition, self.reduce_size_per_partition))
+        if bias:
+            self.bias = Parameter(torch.Tensor(self.output_size_per_partition))
+            # Always initialize bias to zero.
+            with torch.no_grad():
+                self.bias.zero_()
+        else:
+            self.register_parameter("bias", None)
+
+        # Initialize weight.
+        self.master_weight = initialize_affine_weight(
+            self.weight,
+            self.out_features,
+            self.reduce_size,
+            init_method,
+            stride=stride,
+            return_master_weight=keep_master_weight_for_test,
+            partition_strategy=[self.output_size_per_partition, self.reduce_size_per_partition]
+        )
 
         #TODO:
         #1. how do we partition this kernel
         #2. Initialize weight? randomly ??
         #3. Input partition inside??
+        #4. Do we need processor layout, I mean how to make it 2D or 3D??
+
 
     def forward(self, input_: torch.Tensor) -> torch.Tensor:  # type:ignore
-        return None
+        if self.input_is_parallel:
+            input_parallel = input_
+        else:
+            # here we have big issue
+            # how to distribute input if it has not been split
+            # we need input_size_per_partition and reduce_size_per_partition
+            input_parallel = scatter_to_model_parallel_region(input_)
+
+        # Matrix multiply.
+        output_parallel = F.linear(input_parallel, self.weight)
+        if self.parti_reduce_features != 1:
+            # TODO: need to all-reduce
+            output_ = None
+
+        if self.bias is not None:
+            output = output_ + self.bias
+        else:
+            output = output_
+        return output
--- fairscale/nn/op/utils.py
+++ fairscale/nn/op/utils.py
@@ -34,3 +34,40 @@ def split_tensor_along_last_dim(
         return tuple(chunk.contiguous() for chunk in tensor_list)
 
     return tensor_list
+
+
+def initialize_affine_weight(
+    weight: torch.Tensor,
+    out_features: int,
+    reduce_size: int,
+    init_method: Callable[[torch.Tensor], torch.Tensor],
+    stride: int = 1,
+    return_master_weight: bool = False,
+    partition_size_list: List[int] = [1, 1],  #out_dim, reduction_dim, Here is transposed
+) -> Optional[torch.Tensor]:
+    """Initialize affine weight for distribution."""
+    # If we only use 1 process for model parallelism, bypass scatter.
+    world_size = get_model_parallel_world_size()
+    if world_size == 1:
+        init_method(weight)
+        if return_master_weight:
+            return weight
+        return None
+
+    # Initialize master weight
+    master_weight = torch.empty(out_features, reduce_size, dtype=weight.dtype, requires_grad=False)
+    init_method(master_weight)
+
+    #partition large master weight into small piece (partition_size_list[0] x partition_size_list[1])
+    weight_list = torch.unfold(0, partition_size_list[0], partition_size_list[0])\
+        .unfold(1, partition_size_list[1], partition_size_list[1])
+
+    # TODO: rank how to map 2D processor space??
+    rank = get_model_parallel_rank()
+    local_weight_list = weight_list[??]
+
+    with torch.no_grad():
+        torch.cat(my_weight_list, dim=partition_dim, out=weight)
+    if return_master_weight:
+        return master_weight
+    return None
